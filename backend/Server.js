eef0a5d25d1a78acf218f3744921a199e5dfce78
GujjariSaiKumar0706 <gujjarisaikumar0706@gmail.com>
2024-10-26 20:05:50 +0530

    backend first changes

--- backend/Server.js
+++ backend/Server.js
@@ -5,7 +5,7 @@ const path = require("path")
 require('dotenv').config()
 
 // deploy react build in this server
-app.use(exp.static(path.join(__dirname, "../frontend/build")))
+// app.use(exp.static(path.join(__dirname, "../frontend/build")))
 
 //import mongoclient
 const mongoClient = require("mongodb").MongoClient;
@@ -23,19 +23,13 @@ app.use(exp.json());
 mongoClient.connect(process.env.DB_URL)
   .then(client => {
     //get db obj
-    const JPMC = client.db('JPMC');
+    const nutrinest = client.db('nutrinest');
     //get collection obj
-    const studentcollections = JPMC.collection("studentcollections")
-    const instructorcollections=JPMC.collection("instructorcollections");
-    const instructorreplicatecollections=JPMC.collection("instructorreplicatecollections");
-    const parentcollections=JPMC.collection("parentcollections"); 
-    const admincollections=JPMC.collection("admincollections");
+    const userscollection = nutrinest.collection("userscollection")
+    const foodcollection=nutrinest.collection("foodcollection");
       //share collection obj with exp app
-      app.set("studentcollections",studentcollections);
-      app.set("instructorcollections",instructorcollections);
-      app.set("parentcollections",parentcollections);
-      app.set("instructorreplicatecollections",instructorreplicatecollections);
-      app.set("admincollections",admincollections)
+      app.set("userscollection",userscollection);
+      app.set("foodcollection",foodcollection);
       //confirm connection status
       console.log("DB is connected");
   })
@@ -45,18 +39,16 @@ mongoClient.connect(process.env.DB_URL)
 
 
 //import api routes
-const instructorApp = require("./APIS/instructorapi")
-const parentApp = require("./APIS/parentapi")
-const adminApp = require("./APIS/adminapi")
+const userApp = require("./APIS/usersapi")
+const foodApp = require("./APIS/foodapi")
 
 //if patr starts with user-api,send request to userApi
-app.use("/instructor-api",instructorApp)
-app.use("/parent-api",parentApp)
-app.use("/admin-api",adminApp)
+app.use('/user-api',userApp)
+app.use('/food-api',foodApp)
 
-app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, "../frontend/build/index.html"))
-})
+// app.use((req, res, next) => {
+//   res.sendFile(path.join(__dirname, "../frontend/build/index.html"))
+// })
 
 
 
@@ -65,4 +57,4 @@ app.use((err, req, res, next) => {
   res.send({message:err.message});
 })
 const port=process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server is running on port 8000...`))
+app.listen(port, () => console.log(`Server is running on port ${port}`))
--- frontend/src/components/admin/Admin.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import AdminApproval from './AdminApproval'
-function Admin() {
-  return (
-    <AdminApproval/>
-  )
-}
-
-export default Admin
